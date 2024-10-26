55507ac45182ded8ee35adc92ead1a8deb460ce1
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-10-26 13:25:16 -0400

    Implemented update() based on Game of Life rules

--- node.py
+++ node.py
@@ -3,6 +3,9 @@ This file defines the Node class for our program.
 """
 
 class Node:
+    """
+    A class representing a 1km x 1km square of a map.
+    """
     state: int
     temp: float
     elevation: int
@@ -17,4 +20,17 @@ class Node:
         self.neighbours = neighbours
 
     def update(self):
-        raise NotImplementedError
+        """
+        Update current node based on the state of neighbouring nodes using Game of Life rules.
+        """
+        burningNeighbours = 0
+        for neighbour in self.neighbours:
+            if neighbour.state == 1:
+                burningNeighbours += 1
+
+        if self.state == 0:
+            if burningNeighbours == 3:
+                self.state = 1
+        else:
+            if burningNeighbours < 2 or burningNeighbours > 3:
+                self.state = 0
