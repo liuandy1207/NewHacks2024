a82a98b8d8c5f38d068ac6811248460df9744193
Jinny Yoo <hyejin.yoo@mail.utoronto.ca>
2024-10-26 14:17:07 -0400

    grid changes

--- grid.py
+++ grid.py
@@ -1,6 +1,6 @@
 import matplotlib as mpl
 from fontTools.merge import cmap
-from matplotlib import pyplot, pyplot as plt
+from matplotlib import pyplot, pyplot as plt, colors
 import numpy as np
 
 "Grid of the simulation"
@@ -11,19 +11,28 @@ class Grid:
         self.column = column
 
     def create_grid(self):
+        # Create a 10x10 grid with all values set to a single level, for example, 0
+        data = np.zeros((self.row, self.column))
 
-        zvals = np.random.rand(self.row, self.column) * -2
+        # Set up the colormap with a single default color
+        cmap = colors.ListedColormap(['lightgray'])
+        bounds = [0,1]  # Boundaries for single color
+        norm = colors.BoundaryNorm(bounds, cmap.N)
 
-        # make a color map of fixed colors
-        cmap = mpl.colors.ListedColormap(['white', 'black', 'red'])
-        bounds = [-6, -2, 2, 6]
-        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+        # Plot the initial grid with one color
+        fig, ax = plt.subplots()
+        ax.imshow(data, cmap=cmap, norm=norm)
 
-        # tell imshow about color map so that only set colors are used
-        img = pyplot.imshow(zvals, interpolation='nearest',
-                            cmap="Blues", norm=norm)
+        # Draw gridlines for clarity
+        ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=0.5)
+        ax.set_xticks(np.arange(0, self.column, 10))
+        ax.set_yticks(np.arange(0, self.row, 10))
 
-        pyplot.show()
-ngrid = Grid(1000,1000)
+        plt.show()
+
+        # Later on, to change colors, you can update `data` with new values
+        # and replot with a new colormap or colors for each value.
+
+ngrid = Grid(500,500)
 ngrid.create_grid()
 
