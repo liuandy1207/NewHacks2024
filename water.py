fe04da5855eef34807f014dd0751da649d7511cc
liuandy1207 <liuandy1207@gmail.com>
2024-10-27 10:33:07 -0400

    prevented water from catchin on fire

--- water.py
+++ water.py
@@ -4,7 +4,7 @@ class Water(Terrain):
 
     def __init__(self):
         self.fuel = 0
-        self.ignitionTemp = 100000
+        self.ignitionTemp = 999999999999999999999999999999999999999999999999999
 
     @staticmethod
     def updateTempFuel(node):
